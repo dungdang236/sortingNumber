34a0b7738ae79d84f83c8d51883dce351e8e9bde
dungdang236 <dungdang4593@gmail.com>
2015-12-09 09:30:45 -0800

    Initial Commit

---
+++ sortingNumber/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sortingNumber
+//
+//  Created by student on 09/12/2015.
+//  Copyright © 2015 dungdang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sortingNumber/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sortingNumber
+//
+//  Created by student on 09/12/2015.
+//  Copyright © 2015 dungdang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
